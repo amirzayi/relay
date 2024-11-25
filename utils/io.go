73be234a0671c5bac54dd06f2a0d975eac634479
amir <mirzayi994@gmail.com>
2024-11-25 14:53:04 +0330

    refactor: remove duplicated codes

--- utils/io.go
+++ utils/io.go
@@ -7,6 +7,18 @@ import (
 
 // DrawRWProgressbar reads data from an io.Reader and writes it to an io.Writer with buffering while displaying a progress bar.
 func DrawRWProgressbar(r io.Reader, w io.Writer, size int64, bufferSize, barSize int, barTitle string) error {
+	return drawRWProgressbar(r, w, size, bufferSize, func(syncedBytes int64) {
+		donePercent := int(syncedBytes * 100 / size)
+		drawProgressBar(donePercent, barSize, barTitle)
+	})
+}
+
+// WriteFromReader reads data from an io.Reader and writes it to an io.Writer with buffering.
+func WriteFromReader(r io.Reader, w io.Writer, size int64, bufferSize int) error {
+	return drawRWProgressbar(r, w, size, bufferSize, nil)
+}
+
+func drawRWProgressbar(r io.Reader, w io.Writer, size int64, bufferSize int, progressFn func(syncedBytes int64)) error {
 	lr := io.LimitReader(r, size)
 	rw := io.TeeReader(lr, w)
 
@@ -15,27 +27,11 @@ func DrawRWProgressbar(r io.Reader, w io.Writer, size int64, bufferSize, barSize
 	for syncedBytes < size {
 		n, err := rw.Read(buffer)
 		if err != nil {
-			return fmt.Errorf("failed to sync buffer between network and disk: %v", err)
+			return fmt.Errorf("\r\nfailed to sync buffer between network and disk: %v", err)
 		}
-
 		syncedBytes += int64(n)
-		donePercent := int(syncedBytes * 100 / size)
-		drawProgressBar(donePercent, barSize, barTitle)
-	}
-	return nil
-}
-
-// WriteFromReader reads data from an io.Reader and writes it to an io.Writer with buffering.
-func WriteFromReader(r io.Reader, w io.Writer, size int64, bufferSize int) error {
-	lr := io.LimitReader(r, size)
-	rw := io.TeeReader(lr, w)
-
-	buffer := make([]byte, bufferSize)
-	totalBytesRead := int64(0)
-	for totalBytesRead < size {
-		_, err := rw.Read(buffer)
-		if err != nil && err != io.EOF {
-			return fmt.Errorf("failed to sync buffer between network and disk: %v", err)
+		if progressFn != nil {
+			progressFn(syncedBytes)
 		}
 	}
 	return nil
