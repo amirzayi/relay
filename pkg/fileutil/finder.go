5dd73f02ff965ecca1ffd7e784bf7c760d4e07c9
amir <mirzayi994@gmail.com>
2026-05-06 18:42:42 +0330

    refactor(file): use std library to find

--- pkg/fileutil/finder.go
+++ pkg/fileutil/finder.go
@@ -1,79 +1,26 @@
 package fileutil
 
 import (
-	"fmt"
-	"os"
+	"io/fs"
 	"path/filepath"
+	"strings"
 )
 
 func GetFilesByPaths(paths ...string) (Files, error) {
-	files := []File{}
-
-	for _, path := range paths {
-		fileInfo, err := os.Stat(path)
-		if err != nil {
-			return nil, fmt.Errorf("failed to read information of %s, %v", path, err)
-		}
-
-		if !fileInfo.IsDir() {
+	var files Files
+	for _, pathDir := range paths {
+		err := filepath.Walk(pathDir, func(path string, info fs.FileInfo, err error) error {
 			files = append(files, File{
-				Name:    fileInfo.Name(),
-				Size:    fileInfo.Size(),
+				Name:    info.Name(),
+				Size:    info.Size(),
 				Path:    path,
-				Parents: nil,
+				Parents: strings.Split(strings.TrimPrefix(strings.TrimSuffix(path, filepath.Base(path)), pathDir), string(filepath.Separator)),
 			})
-			continue
-		}
-
-		dirFiles, err := readDirectoryFilesRecursively(path, filepath.Base(path))
-		if err != nil {
-			return nil, fmt.Errorf("failed to retrieve files on directory %s, %v", path, err)
-		}
-		files = append(files, dirFiles...)
-	}
-
-	return files, nil
-}
-
-// readDirectoryFilesRecursively retrieve directory and subdirectories files
-func readDirectoryFilesRecursively(path string, parents ...string) (Files, error) {
-	entries, err := os.ReadDir(path)
-	if err != nil {
-		return nil, fmt.Errorf("failed to retrieve directory data on %s, %v", path, err)
-	}
-
-	var files []File
-
-	// we need to separate files and directories iteration over entries because of confusing
-	for _, entry := range entries {
-		if entry.IsDir() {
-			continue
-		}
-
-		entryInfo, err := entry.Info()
-		if err != nil {
-			return nil, fmt.Errorf("failed to load info of %s, %v", entry.Name(), err)
-		}
-
-		files = append(files, File{
-			Name:    entry.Name(),
-			Size:    entryInfo.Size(),
-			Path:    filepath.Join(path, entry.Name()),
-			Parents: parents,
+			return err
 		})
-	}
-
-	for _, entry := range entries {
-		if !entry.IsDir() {
-			continue
-		}
-		parents = append(parents, entry.Name())
-		innerFiles, err := readDirectoryFilesRecursively(filepath.Join(path, entry.Name()), parents...)
 		if err != nil {
-			return nil, err
+			return files, err
 		}
-		files = append(files, innerFiles...)
 	}
-
 	return files, nil
 }
